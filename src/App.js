b96632e1fc520061914d492efff7647c7a610738
christian <cristhho@gmail.com>
2020-08-22 00:56:59 -0500

    categories list component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyles'
+
+export default function App () {
+  return (
+    <>
+      <GlobalStyle />
+      <ListOfCategories />
+    </>
+  )
+}
---
+++ src/components/ListOfCategories/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const List = styled.ul`
+  display: flex;
+  overflow-y: hidden;
+  overflow-x: scroll;
+  width: 100%;
+  padding-bottom: 10px;
+  scrollbar-width: thin;
+  scrollbar-color: #90A4AE #CFD8DC;
+  &::-webkit-scrollbar{
+    height: 16px;
+  }
+  &::-webkit-scrollbar-track{
+    background: #CFD8DC;
+  }
+  &::-webkit-scrollbar-thumb{
+    background-color: #90A4AE;
+    border-radius: 25px;
+    border: 4px solid #CFD8DC;
+  }
+`
+
+export const Item = styled.li`
+  padding: 0 8px;
+`
