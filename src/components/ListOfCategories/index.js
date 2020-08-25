72077aa4f64423e8cb3eb33e4e1b36abc03b58a5
cristhian ochoa <cristhho@gmail.com>
2020-08-25 01:07:38 -0500

    custom hook

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -3,15 +3,22 @@ import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => {
+const useCategoriesData = () => {
   const [categories, setCategories] = useState([])
-  const [showFixed, setShowFixed] = useState(false)
-
   useEffect(() => {
     window.fetch('https://petgram-api-black.vercel.app/categories')
       .then((res) => res.json())
-      .then((data) => setCategories(data))
+      .then((data) => {
+        setCategories(data)
+      })
   }, [])
+
+  return { categories }
+}
+
+export const ListOfCategories = () => {
+  const { categories } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(() => {
     const onScroll = (e) => {
