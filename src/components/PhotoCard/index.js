ef54c299d625ef89dc74fa78fe00af7785998f0c
cristhian ochoa <cristhho@gmail.com>
2020-08-27 23:05:11 -0500

    intersection observer support for old browsers

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -8,16 +8,21 @@ export const PhotoCard = ({ id, likes = 0, src = 'https://res.cloudinary.com/mid
   const [show, setShow] = useState(false)
 
   useEffect(() => {
-    const observer = new window.IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0]
-      console.log(isIntersecting)
-      if (isIntersecting) {
-        setShow(true)
-        observer.disconnect()
-      }
-    })
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined'
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver((entries) => {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
 
-    observer.observe(ref.current)
+      observer.observe(ref.current)
+    })
   }, [ref])
 
   return (
