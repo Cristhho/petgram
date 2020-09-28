3065b756892aed9d3d03cea613fae2b482f6a451
cristhian ochoa <cristhho@gmail.com>
2020-09-27 22:59:41 -0500

    protect routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+/* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable operator-linebreak */
 import React from 'react'
 import { Router } from '@reach/router'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegistered } from './pages/NotRegistered'
 import { Logo } from './components/Logo'
 import { Navbar } from './components/Navbar'
 import { GlobalStyle } from './styles/GlobalStyles'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export default function App () {
   return (
@@ -18,6 +26,19 @@ export default function App () {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth ? <Router>
+              <Favs path='/fav' />
+              <User path='/user' />
+            </Router>
+              : <Router>
+                <NotRegistered path='/fav' />
+                <NotRegistered path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <Navbar />
     </>
   )
---
+++ src/pages/NotRegistered.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NotRegistered = () => {
+  return (
+    <h1>Not registered</h1>
+  )
+}
