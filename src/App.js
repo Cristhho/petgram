cfac86645af859a270c20b8693a2cb8cc37166b6
christian <cristhho@gmail.com>
2020-10-19 16:49:32 -0500

    not found, redirects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable operator-linebreak */
-import React from 'react'
-import { Router } from '@reach/router'
+import React, { useContext } from 'react'
+import { Redirect, Router } from '@reach/router'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
@@ -12,31 +12,27 @@ import { Logo } from './components/Logo'
 import { Navbar } from './components/Navbar'
 import { GlobalStyle } from './styles/GlobalStyles'
 import Context from './Context'
+import { NotFound } from './pages/NotFound'
 
 export default function App () {
+  const { isAuth } = useContext(Context.Context)
   return (
     <>
       <GlobalStyle />
       <Logo />
       <div id='wrapper'>
         <Router>
+          <NotFound default />
           <Home path='/' />
           <Home path='/pet/:id' />
           <Detail path='/detail/:detailId' />
+          {!isAuth && <NotRegistered path='/login' />}
+          {!isAuth && <Redirect noThrow from='/fav' to='/login' />}
+          {!isAuth && <Redirect noThrow from='/user' to='/login' />}
+          {isAuth && <Redirect noThrow from='/login' to='/' />}
+          <Favs path='/fav' />
+          <User path='/user' />
         </Router>
-        <Context.Consumer>
-          {
-            ({ isAuth }) =>
-              isAuth ? <Router>
-                <Favs path='/fav' />
-                <User path='/user' />
-              </Router>
-                : <Router>
-                  <NotRegistered path='/fav' />
-                  <NotRegistered path='/user' />
-                </Router>
-          }
-        </Context.Consumer>
       </div>
       <Navbar />
     </>
