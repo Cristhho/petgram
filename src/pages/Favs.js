343d82a31fc434973c489510346e28bf4dd02477
christian <cristhho@gmail.com>
2020-10-20 17:35:05 -0500

    react helmet

--- src/pages/Favs.js
+++ src/pages/Favs.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
+
 import { FavsWithQuery } from '../container/GetFavorites'
 
 export const Favs = () => {
   return (
     <>
+      <Helmet>
+        <title>Petgram - Tus favoritos</title>
+      </Helmet>
       <h1>Favoritos</h1>
       <FavsWithQuery />
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotos } from '../container/ListOfPhotos'
@@ -6,6 +7,10 @@ import { ListOfPhotos } from '../container/ListOfPhotos'
 export const Home = ({ id }) => {
   return (
     <>
+      <Helmet>
+        <title>Petgram - Tu app de fotos de mascotas</title>
+        <meta name='description' content='Con Petgram puedes compartir fotos de tus animales favoritos' />
+      </Helmet>
       <ListOfCategories />
       <ListOfPhotos categoryId={id} />
     </>
